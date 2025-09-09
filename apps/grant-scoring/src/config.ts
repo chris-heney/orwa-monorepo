5f73f49a68408707d7b9c37e9287239c76ac7dd9
Chris Heney <chris.heney@gmail.com>
2025-09-09 17:21:59 -0500

    Added dev scripts.

--- apps/grant-scoring/src/config.ts
+++ apps/grant-scoring/src/config.ts
@@ -1,4 +1,4 @@
 // @TODO: Check node.env == production instaed of true:
 export const API_ENDPOINT:string = import.meta.env.VITE_API_ENDPOINT
-export const API_TOKEN:string = import.meta.env.VITE_API_TOKEN
+export const API_KEY:string = import.meta.env.VITE_API_KEY
 export const BASE_URI:string = import.meta.env.VITE_BASE_URI
